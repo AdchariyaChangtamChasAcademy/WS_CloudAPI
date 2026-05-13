ba7f0722f27033553d45e1f3bf6a67307058b970
AdchariyaChangtamChasAcademy <adchariya.changtam@chasacademy.com>
2026-05-13 14:37:13 +0200

    azure name fixes

--- CloudNativeApp.Api/obj/Debug/net9.0/CloudNativeApp.Api.AssemblyInfo.cs
+++ CloudNativeApp.Api/obj/Debug/net9.0/CloudNativeApp.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CloudNativeApp.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+784f69fdefd187ad117b4b459d6d2a55c5a25b29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e3de6392f3a8323f5489d9ed0499a1ce8f66cc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CloudNativeApp.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CloudNativeApp.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
