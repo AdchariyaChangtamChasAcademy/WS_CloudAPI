a42c5aa9a7964ae9f63f39920e359eaacd5853b4
AdchariyaChangtamChasAcademy <adchariya.changtam@chasacademy.com>
2026-05-18 13:41:30 +0200

    Azure Key Vaults

--- CloudNativeApp.Api/Program.cs
+++ CloudNativeApp.Api/Program.cs
@@ -1,8 +1,18 @@
+using Azure.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
+
+// Azure Key Vault
+var keyVaultUrl = builder.Configuration["KeyVaultUrl"];
+
+if (!string.IsNullOrEmpty(keyVaultUrl))
+{
+    builder.Configuration.AddAzureKeyVault( new Uri(keyVaultUrl), new DefaultAzureCredential());
+}
 
 var app = builder.Build();
 
